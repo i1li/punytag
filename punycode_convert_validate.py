62832e02fe35a1c70a4e5bb4316150b7c22a405e
i1li <74943880+i1li@users.noreply.github.com>
2024-01-25 17:53:17 -0600

    Update punycode_convert_validate.py
    fixed invalid unicode from showing the utf codes

--- punycode_convert_validate.py
+++ punycode_convert_validate.py
@@ -1,16 +1,15 @@
 import pandas as pd
-import emoji
-import punycode
+import idna
 import re
 def punycode_convert_validate(punycode_str):
     if punycode_str.startswith("xn--"):
         try:
             decoded = punycode_str.encode('ascii').decode('idna', errors='strict')
-            return emoji.emojize(decoded), 'yes'  
+            return decoded, 'yes'  
         except UnicodeError:  
             try:
-                unicode_str = punycode.convert(punycode_str)
-                return emoji.emojize(unicode_str), 'no'
+                unicode_str = idna.decode(punycode_str)
+                return unicode_str, 'no'
             except Exception as e:
                 error_message = str(e)
                 unicode_match = re.search(r"'([^']*)'", error_message)
@@ -22,11 +21,12 @@ def punycode_convert_validate(punycode_str):
         return '', ''
 file_path = 'Namebase-domains-export.csv'
 df = pd.read_csv(file_path)
-df.insert(1, 'unicode', '')
-df.insert(2, 'valid_punycode', '')
 punycode_info = df['name'].apply(punycode_convert_validate)
-df['unicode'] = [info[0] for info in punycode_info]
-df['valid_punycode'] = [info[1] for info in punycode_info]  
+df['unicode'] = [re.sub(r'(?:\\x[\da-fA-F]{2})+|\\u(?:[\da-fA-F]{4})+', '', info[0]) if info[0] else '' for info in punycode_info]
+df.loc[df['unicode'] == df['name'], 'unicode'] = ''
+if 'valid_punycode' not in df.columns:
+    df.insert(2, 'valid_punycode', '')
+df['valid_punycode'] = [info[1] for info in punycode_info]
 if 'name' in df.columns:
     output_file_path = 'Namebase-domains-export.csv'
     df.to_csv(output_file_path, index=False)
